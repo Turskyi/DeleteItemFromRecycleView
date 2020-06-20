0e471ec87adbb62f30df75bedaf9315a409e0ed4
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-06-20 20:48:59 +0300

    update

--- app/src/main/java/com/example/commonmistakes/ItemView.java
+++ app/src/main/java/com/example/commonmistakes/ItemView.java
@@ -1,7 +1,7 @@
 package com.example.commonmistakes;
 
 import android.content.Context;
-import android.support.constraint.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 
